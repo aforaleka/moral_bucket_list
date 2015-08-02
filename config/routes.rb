0887a639a63d5bd9ca98191a3fd869debe13cead
vickiliu8 <vickiliu@umich.edu>
2015-08-02 17:11:12 +0200

    added fname and last name field to user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+ 
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root 'welcome#index'
   get '/dash', to: 'welcome#dash'
   
