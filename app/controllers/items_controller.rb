5a7f8d25862c3d337b3148b54c496be8b963758b
aforaleka <aforaleka@gmail.com>
2015-08-02 18:36:05 +0200

    added virtues and items controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,64 @@
+class ItemsController < ApplicationController
+  
+  def index
+  	@items = Item.all.order(created_at: :desc)
+  	# Item.where(public: false).each do |item|
+  	# 	if item.user != current_user
+  	# 		@items = @items - [item]
+  	# 	end
+  	# end
+  end
+
+  # def myitems
+  # 	@items = current_user.items.order(created_at: :desc)
+  # end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      render 'new'
+    end
+  end
+
+
+  def show
+    @item = Item.find params[:id]
+
+  end
+
+  def edit
+    @item = Item.find params[:id]
+  end
+
+  def update
+    @item = Item.find params[:id]
+    if @item.update item_params
+      redirect_to @item
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item = Item.find params[:id]
+
+  end
+
+
+  private
+
+  
+
+    def item_params
+      params.require(:item).permit(:public, :name)
+    end
+
+
+
+end
---
+++ app/controllers/virtues_controller.rb
@@ -0,0 +1,81 @@
+class VirtuesController < ApplicationController
+  
+  def index
+  	@virtues = Virtue.all.order(created_at: :desc)
+  	# Virtue.where(public: false).each do |virtue|
+  	# 	if virtue.user != current_user
+  	# 		@virtues = @virtues - [virtue]
+  	# 	end
+  	# end
+  end
+
+  # def myvirtues
+  # 	@virtues = current_user.virtues.order(created_at: :desc)
+  # end
+
+  def new
+    @virtue = Virtue.new
+  end
+
+  def create
+    @virtue = current_user.virtues.build(virtue_params)
+    if @virtue.save
+      redirect_to @virtue
+    else
+      render 'new'
+    end
+  end
+
+
+  def show
+    @virtue = Virtue.find params[:id]
+    if (@virtue.user != current_user) && (@virtue.public == false)
+      redirect_to virtues_path
+    end
+
+  end
+
+  def edit
+    @virtue = Virtue.find params[:id]
+    verify_virtue_owner
+  end
+
+  def update
+    @virtue = Virtue.find params[:id]
+    if @virtue.update virtue_params
+      redirect_to @virtue
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @virtue = Virtue.find params[:id]
+
+    if @virtue.user != current_user
+        redirect_to virtue_path
+        flash.alert = "Invalid Permissions"
+    else 
+      @virtue.destroy
+      redirect_to virtues_path
+      flash.notice = "Virtue successfully deleted"
+    end
+  end
+
+
+  private
+
+    def verify_virtue_owner
+      if @virtue.user != current_user
+        redirect_to virtue_path
+        flash.alert = "Invalid Permissions"
+      end
+    end
+
+    def virtue_params
+      params.require(:virtue).permit(:public, :title, :description)
+    end
+
+
+
+end
