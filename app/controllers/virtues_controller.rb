350b79a9ccdf9c33d98fd41706b11c5706db21c1
vickiliu8 <vickiliu@umich.edu>
2015-08-03 13:52:23 +0200

    add and delete and edit virtues

--- app/controllers/virtues_controller.rb
+++ app/controllers/virtues_controller.rb
@@ -22,14 +22,14 @@ class VirtuesController < ApplicationController
 
   def edit
     verify_virtue_owner
-    @virtue = Virtue.find params[:id]
+    @virtue = Virtue.find(params[:id])
     
   end
 
   def update
     verify_virtue_owner
     @user = current_user
-    @virtue = Virtue.find params[:id]
+    @virtue = Virtue.find(params[:id])
     if @virtue.update(virtue_params)
       redirect_to profile_path
     else
@@ -43,6 +43,7 @@ class VirtuesController < ApplicationController
   private
 
     def verify_virtue_owner
+      @virtue = Virtue.find(params[:id])
       if @virtue.user_id != current_user.id
         redirect_to virtue_path
         flash.alert = "Invalid Permissions"
