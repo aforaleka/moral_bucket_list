904a4504c9cb20076f5f10ec76e5a68cc41e186f
vickiliu8 <vickiliu@umich.edu>
2015-08-03 11:51:23 +0200

    added create a new virtue

--- app/controllers/virtues_controller.rb
+++ app/controllers/virtues_controller.rb
@@ -1,66 +1,43 @@
 class VirtuesController < ApplicationController
-  
-  def index
-  	@virtues = Virtue.all.order(created_at: :desc)
-  	# Virtue.where(public: false).each do |virtue|
-  	# 	if virtue.user != current_user
-  	# 		@virtues = @virtues - [virtue]
-  	# 	end
-  	# end
-  end
-
-  # def myvirtues
-  # 	@virtues = current_user.virtues.order(created_at: :desc)
-  # end
-
-  def new
-    @virtue = Virtue.new
-  end
-
   def create
-    @virtue = current_user.virtues.build(virtue_params)
-    if @virtue.save
-      redirect_to @virtue
-    else
-      render 'new'
-    end
+    @user = current_user
+    @virtue = @user.virtues.create(virtue_params)
+    redirect to profile_path
   end
 
+  def destroy
 
-  def show
     @virtue = Virtue.find params[:id]
-    if (@virtue.user != current_user) && (@virtue.public == false)
-      redirect_to virtues_path
-    end
 
+    if @virtue.user_id != current_user.id
+        redirect_to user_path(User.find(@virtue.user_id))
+        flash.alert = "Invalid Permissions"
+    else 
+      @virtue.destroy
+      redirect_to profile_path
+      flash.notice = "Virtue successfully deleted"
+    end
   end
+
 
   def edit
-    @virtue = Virtue.find params[:id]
     verify_virtue_owner
+    @virtue = Virtue.find params[:id]
+    
   end
 
   def update
+    verify_virtue_owner
+    @user = current_user
     @virtue = Virtue.find params[:id]
-    if @virtue.update virtue_params
-      redirect_to @virtue
+    if @virtue.update(virtue_params)
+      redirect_to profile_path
     else
       render 'edit'
     end
   end
 
-  def destroy
-    @virtue = Virtue.find params[:id]
-
-    if @virtue.user != current_user
-        redirect_to virtue_path
-        flash.alert = "Invalid Permissions"
-    else 
-      @virtue.destroy
-      redirect_to virtues_path
-      flash.notice = "Virtue successfully deleted"
-    end
-  end
+  
 
 
   private
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:index]
 
 	def index
-		@items = Item.where(:public => true:).order(created_at: :desc)
+		
 	end
 
 	def dash
