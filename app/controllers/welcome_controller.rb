45c2d536ac08c240f99000d82bc691961f45868d
aforaleka <aforaleka@gmail.com>
2015-08-03 11:49:06 +0200

    styling and welcome index options

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,9 @@ class WelcomeController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:index]
 
 	def index
-		@items = Item.where(:public => true:).order(created_at: :desc)
 	end
 
 	def dash
-
 	end
 
 end
