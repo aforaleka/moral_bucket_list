598570f6e51169611c68626da5ad9243b3624179
vickiliu8 <vickiliu@umich.edu>
2015-08-05 10:57:57 +0200

    added friend news feed and nav links

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,11 +2,29 @@ class EventsController < ApplicationController
 	
 
 	def friends
-
+		@friends = current_user.all_following
+		@events = get_followed_events(@friends)
+		@events = @events.sort_by{ |event| event.created_at}.reverse
 	end
 
 	def public
 		@events = Event.all.order(created_at: :desc)
+	end
+
+	private
+
+	def get_followed_events(friends)
+		friend_events = Array.new
+		if (friends.count == 0)
+			return friend_events
+		else
+			friends.each do |f|
+				f.events.each do |event|
+					friend_events << event 
+				end
+			end
+			return friend_events
+		end
 	end
 
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
   belongs_to :user
   belongs_to :act
+  acts_as_followable
 end
